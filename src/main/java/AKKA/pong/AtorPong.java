dee76380eb282568ad078f6e251a534ec6de11dd
daniel.ranghetti <danielranghetti48@gmail.com>
2020-04-02 13:32:24 -0300

    tirado metodos a mais

--- src/main/java/AKKA/pong/AtorPong.java
+++ src/main/java/AKKA/pong/AtorPong.java
@@ -15,13 +15,7 @@ public class AtorPong extends UntypedAbstractActor implements Serializable {
     private ActorSelection atorPing = getContext().actorSelection("akka.tcp://AkkaRemotePing@127.0.0.1:2558/user/AtorPing");
 
     public void onReceive(Object msg) throws Exception {
-        if (msg instanceof Mensagem.Iniciar) {
-//            loggingAdapter.info("Iniciando o ping-pong");
-//            //setando a msg do AtorPing para a AtorPong
-//            atorPing.tell(new Mensagem.PingMsg("Ping novo"), getSelf());
-//            //verificar ser a msg é do AtorPong
-            loggingAdapter.info("ator recebeu inicar quando não deveria");
-        } else if (msg instanceof Mensagem.PingMsg) {
+       if (msg instanceof Mensagem.PingMsg) {
             //pegar a mensagem do AtorPing
             Mensagem.PingMsg atorPing = (Mensagem.PingMsg) msg;
             //mostra a mensagem enviado do AtorPing para o AtorPong no console
